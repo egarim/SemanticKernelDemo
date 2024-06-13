487f7a27ee9a20b20e5466380c7bd46f0acc5ffc
Jose Ojeda <joche.ojeda@bitframeworks.com>
2024-06-13 10:25:38 +0300

    Memory working

--- ConsoleChat/Program.cs
+++ ConsoleChat/Program.cs
@@ -32,9 +32,11 @@ class Program
 Chatbot can have a conversation with you about any topic.
 It can give explicit information or say 'I don't know' if it doesn't have an answer.
  
+{{$myhistory}}
 User:{{$userInput}}
 ChatBot:";
 
+        string history="";
         // 5. Create a Symentic Function - An AI function
         var chatFunction = kernel.CreateFunctionFromPrompt(prompt, 
             executionSettings: new OpenAIPromptExecutionSettings { MaxTokens = 2000, Temperature = 0.7, TopP = 0.5 });
@@ -57,7 +59,10 @@ ChatBot:";
 
                         // Process the user message and get an answer
                         var answer = await chatFunction.InvokeAsync(kernel, arguments);
-
+                        // Append the new instructions to the chat history
+                        var result = $"\nUser: {input}\nAI: {answer}\n";
+                        history += result;
+                        arguments["history"] = history;
                         Console.WriteLine(answer);
                 };
 
